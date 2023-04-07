3ab103bf8aa1e102c43809ffa47f35b10fed283c
David Moulton <dbmoulton@gmail.com>
2023-04-06 22:40:19 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("https://dog.ceo/api/breeds/image/random/4") 
+        .then(
+        (response)=>
+        response.json()
+        )
+        .then ((data) => {
+            data.message.forEach(image => {
+            let dogImgEl = document.createElement("img")
+            dogImgEl.src = image
+            let dogImgDiv = document.getElementById("dog-image-container")
+            dogImgDiv.appendChild(dogImgEl);
+            })
+        })
+        .catch (err=>console.log(err))
+    fetch("https://dog.ceo/api/breeds/list/all")
+    .then(res => {
+        return res.json();
+    })
+    .then(data => {
+        // console.log(data);
+        Object.keys(data.message).forEach(breed =>{
+            console.log(breed)
+            let listEl = document.createElement("li")
+            listEl.innerText = breed
+            document.getElementById("dog-breeds").append(listEl)
+        })
+    })
+})
